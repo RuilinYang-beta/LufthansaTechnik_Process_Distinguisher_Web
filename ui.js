738c86ede9a5c237202ea592310a625051c188fa
Ruilin Yang <ruilin.yang@protonmail.com>
2022-02-04 13:53:37 +0100

    clear working tree

--- ui.js
+++ ui.js
@@ -2,3 +2,5 @@ $(".hamburger").click(() => {
   $(".navbar").toggleClass("collapse");
   $(".main_container").toggleClass("expand");
 });
+
+$(".task:has(.subtask)").css("border-bottom", "2px solid #ffad00");
